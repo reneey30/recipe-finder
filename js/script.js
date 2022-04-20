e9ecb668fcaa08d51aed15d96c16e1e23ff2b08b
Irene Njeru <irenenjeru8@gmail.com>
2022-04-20 23:46:14 +0300

    add functionality for get recipes button

--- js/script.js
+++ js/script.js
@@ -5,19 +5,93 @@ const API_KEY = "e74950d89dbe4c6a9349da28a66873bd";
 
 const joiner = ",+"
 
-let recipeID = 716429;
+let ingredients = [];
 
-let query1 = `${ENDPOINT}findByIngredients`; 
-let query2 = `${ENDPOINT}${recipeID}/information`;
+// appending ingredients to unordered list
+function addIngredient(userIngredient) {
+    let ul = document.getElementById("ingredient-list");
+    let li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.appendChild(document.createTextNode(userIngredient));
+    ul.appendChild(li);
+  }
 
-let ingredients = ["apples", "flour", "sugar"];
-let ingredientString = ingredients[0] + joiner + ingredients[1] + joiner + ingredients[2] + "&number=" + RECIPES_COUNT;
+// getting list of ingredients
+function getIngredients(){
+    let listOfIngredients = document.getElementById("ingredient-list").getElementsByTagName("li");
+    
+    for (let item of listOfIngredients) {
+        console.log(item.innerText);
+        ingredients.push(item.innerText);
+        console.log(ingredients)
+    }
+    console.log(ingredients)
 
-let ingredientQuery = `${query1}?ingredients=${ingredientString}&apiKey=${API_KEY}`;
 
-// let ingredientQuery = `${query2}?includeNutrition=false&apiKey=${API_KEY}`;
+    let recipeID = 716429;
 
-console.log(ingredientQuery);
+    let query1 = `${ENDPOINT}findByIngredients`; 
+    let query2 = `${ENDPOINT}${recipeID}/information`;
+
+    // let ingredients = ["apples", "flour", "sugar"];
+    // let ingredients = [];
+    let allIngredients = ingredients.join(joiner);
+
+    console.log(allIngredients);
+
+
+    // let ingredientString = ingredients[0] + joiner + ingredients[1] + joiner + ingredients[2] + "&number=" + RECIPES_COUNT;
+
+    let ingredientString = allIngredients + "&number=" + RECIPES_COUNT;
+
+    let ingredientQuery = `${query1}?ingredients=${ingredientString}&apiKey=${API_KEY}`;
+
+    // let ingredientQuery = `${query2}?includeNutrition=false&apiKey=${API_KEY}`;
+
+    console.log(ingredientQuery);
+
+    fetch(ingredientQuery)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    }
+//   let x = document.getElementById("user-ingredient").value;
+
+
+// button 1
+const buttonAddIngredient = document.getElementById("add-ingredient");
+
+buttonAddIngredient.onclick = function() {addIngredient(document.getElementById("user-ingredient").value)};
+
+
+// button 2
+const buttonGetRecipe = document.getElementById("get-recipes");
+
+buttonGetRecipe.onclick = function() {getIngredients()};
+
+
+  
+
+// let recipeID = 716429;
+
+// let query1 = `${ENDPOINT}findByIngredients`; 
+// let query2 = `${ENDPOINT}${recipeID}/information`;
+
+// // let ingredients = ["apples", "flour", "sugar"];
+// // let ingredients = [];
+// let allIngredients = ingredients.join(joiner);
+
+// console.log(allIngredients);
+
+
+// // let ingredientString = ingredients[0] + joiner + ingredients[1] + joiner + ingredients[2] + "&number=" + RECIPES_COUNT;
+
+// let ingredientString = allIngredients + "&number=" + RECIPES_COUNT;
+
+// let ingredientQuery = `${query1}?ingredients=${ingredientString}&apiKey=${API_KEY}`;
+
+// // let ingredientQuery = `${query2}?includeNutrition=false&apiKey=${API_KEY}`;
+
+// console.log(ingredientQuery);
 
 // fetch(ingredientQuery)
 // .then(res => res.json())

31ad0854bac14a0b6c54004973340bf7dc1da0d4
Irene Njeru <irenenjeru8@gmail.com>
2022-04-21 19:29:38 +0300

    Add the instruction link

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const ENDPOINT = "https://api.spoonacular.com/recipes/";
 const RECIPES_COUNT = 3;
-const API_KEY = "e74950d89dbe4c6a9349da28a66873bd";
+// const API_KEY = "e74950d89dbe4c6a9349da28a66873bd";
+const API_KEY = "9dcdb30197a14d4c973b4501f5bb3c7d";
 
 const joiner = ",+"
 
-let recipes = [];
-
 let ingredients = [];
+let recipeIds = [];
 
 function addList(inputValue, ul) {
     let li = document.createElement("li");
@@ -20,29 +20,29 @@ function addList(inputValue, ul) {
 // getting list of ingredients
 function getIngredients(){
     let listOfIngredients = document.getElementById("ingredient-list").getElementsByTagName("li");
+    list = [];
     
     for (let item of listOfIngredients) {
         
-        ingredients.push(item.innerText);
+        list.push(item.innerText);
         
     }
-    console.log(ingredients)
     
-    return ingredients;
+    return list;
     }
 
     
 function getRecipes () {
 
     ingredients = getIngredients();
-        // let recipeID = 716429;
+    
 
     let query1 = `${ENDPOINT}findByIngredients`; 
     // let query2 = `${ENDPOINT}${recipeID}/information`;
 
     let allIngredients = ingredients.join(joiner);
 
-    console.log(allIngredients);
+    // console.log(allIngredients);
 
     let ingredientString = allIngredients + "&number=" + RECIPES_COUNT;
 
@@ -52,6 +52,7 @@ function getRecipes () {
 
     console.log(ingredientQuery);
 
+    // fetch recipes
     fetch(ingredientQuery)
     .then(res => res.json())
     .then(recipes => {
@@ -62,13 +63,19 @@ function getRecipes () {
         // console.log(cards);
         let i = 0;
 
+        let tempRecipesId = []
+
         for (let recipe of recipes){
-            // working on each individual recipe
+            // working on each individual recipe card
+
+            tempRecipesId.push(recipe.id);
+            
             // title: recipe.title
             // image: recipe.image
             // ingredients: 1. ingredients not requested(missing) recipe.missedIngredients - loop thru => recipe.missedIngredients[n].name
             //ingredients: 2. ingredients requested(in request by user) recipe.usedIngredients - loop thru => recipe.usedIngredients[n].name
-            //instructions: todo
+            //instructions: recipe.id will be used to get {RecipeID} to use in query 2
+            // 
 
             // console.log(recipe);
 
@@ -95,9 +102,107 @@ function getRecipes () {
                 addList(missedIngredient.name, listContainer);
             }
 
+            // get recipe id from recipe results json - recipe.id
+            
+            // console.log(`${ENDPOINT}${recipe.id}/information`);
+
+            // instructionButton = cards[i].getElementsByClassName("instructions")[0];
+
+            // console.log("before: ");
+            // console.log(instructionButton);
+
+            //second fetch for getting recipe steps/instructions
+            // fetch(`${ENDPOINT}${recipe.id}/information?apiKey=${API_KEY}`)
+            // .then(res => res.json())
+            // .then(steps => {
+            //     // appending instructions url to instructions button
+            // // cards[i].getElementsByClassName("instructions")[0].href = steps.spoonacularUrl;
+            // // console.log(cards[i].getElementsByClassName("instructions")[0]);
+            // // console.log(steps.sourceUrl);
+
+            // console.log("inside: ");
+            // console.log(instructionButton);
+
+            // instructionButton.href = steps.sourceUrl;
+            
+            
+            // });
+
+            
+            // console.log("outside: ");
+            // console.log(instructionButton);
+            
+            
+
             i++;     
         }
-    })  
+
+        recipeIds = tempRecipesId;
+        console.log("recipes id inside fetch 1")
+        console.log(recipeIds);
+        return tempRecipesId;
+    }).then(themIds => {
+        console.log("recipes id outside");
+        console.log(themIds);
+        // let j = 0;
+        // fetch instructions
+        // recipeIds = [647524, 648729, 1697653]
+        for (let j=0; j < themIds.length; j++) {
+
+            fetch(`${ENDPOINT}${themIds[j]}/information?apiKey=${API_KEY}`)
+            .then(res => res.json())
+            .then(steps => {
+            console.log(themIds[j]);
+            console.log(steps);
+                // appending instructions url to instructions button
+            // cards[i].getElementsByClassName("instructions")[0].href = steps.spoonacularUrl;
+            // console.log(cards[i].getElementsByClassName("instructions")[0]);
+            // console.log(steps.sourceUrl);
+
+            let cards = document.getElementById("cards-container").getElementsByClassName("recipe");
+
+            
+            cardLink = cards[j].getElementsByClassName("instructions")[0];
+            
+            console.log(cards[j].getElementsByClassName("card-title")[0].innerText);
+            console.log("inside card: " + j);
+            
+            cardLink.href = steps.sourceUrl;
+            console.log(steps.sourceUrl);
+           
+            });
+        
+        }
+    });  
+
+    // console.log("recipes id outside");
+    // console.log(recipeIds);
+    // let j = 0;
+    // // fetch instructions
+    // // recipeIds = [647524, 648729, 1697653]
+    // for (let recipeId of recipeIds) {
+    //     fetch(`${ENDPOINT}${recipeId}/information?apiKey=${API_KEY}`)
+    //     .then(res => res.json())
+    //     .then(steps => {
+    //         // appending instructions url to instructions button
+    //     // cards[i].getElementsByClassName("instructions")[0].href = steps.spoonacularUrl;
+    //     // console.log(cards[i].getElementsByClassName("instructions")[0]);
+    //     // console.log(steps.sourceUrl);
+
+    //     let cards = document.getElementById("cards-container").getElementsByClassName("recipe");
+
+         
+    //     cardLink = cards[j].getElementsByClassName("instructions")[0];
+        
+    //     console.log("inside: ");
+    //     console.log(cardLink);
+    //     cardLink.href = steps.sourceUrl;
+        
+        
+    //     j++;
+    //     });
+    // }
+   
 
     // un hide container b
     let containerB = document.getElementById("container-b");
@@ -129,13 +234,16 @@ function startOver() {
 
     for (let card of cards){
         // clear title
-        card.getElementsByClassName("card-title")[0].innerText = "";
+        card.getElementsByClassName("card-title")[0].innerText = "Recipe Title";
         // clear image url
-        card.getElementsByClassName("card-img-top")[0].src = "";
+        card.getElementsByClassName("card-img-top")[0].src = "https://spoonacular.com/recipeImages/716429-556x370.jpg";
 
         // clear list of ingredients
         let listContainer = card.getElementsByClassName("list-group")[0];
         listContainer.innerHTML = "";
+
+        ingredients = [];
+        recipeIds = [];
     }
     
 
